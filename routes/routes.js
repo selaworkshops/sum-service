d25ccf4909aa4030f6f741cd8fe60fe164554a6c
Leon Jalfon <leonj@sela.co.il>
2018-06-11 20:41:54 +0300

    fix response + refactoring

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,24 @@
 var appRouter = function (app) {
-var sum = require('./../sum.js');
 
 app.get("/", function(req, res) {
 	res.status(404).send("Error 404, To use the sum service browse to: {server}:{port}/sum/{number1}/{number2}");
 })
   
 app.get("/sum/:number1/:number2", function(req,res) {
-    var number1 = req.params.number1;
-    var number2 = req.params.number2;
-    var total = sum.sum(number1,number2);
-    res.send(JSON.stringify(total));
+    var number1 = parseInt(req.params.number1);
+    var number2 = parseInt(req.params.number2);
+    var total = number1 + number2;
+
+	var body = {
+		"first-number": number1,
+		"second-number": number2,
+		"operation": "sum",
+		"total": total
+	}
+	
+	res.setHeader('Access-Control-Allow-Origin', 'null');
+	return res.status(200).end(JSON.stringify(body));
+	  
   });
 }
 
